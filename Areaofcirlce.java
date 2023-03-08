559e3a795504210924a040e8ec7a918b4a527871
Sarveshbagde <sarveshbagde@gmail.com>
2023-03-08 09:59:52 +0530

    Area of circle

---
+++ Areaofcirlce.java
@@ -0,0 +1,13 @@
+class Areaofcirlce 
+{
+public static void main(String args[])
+{
+
+double pai = 3.14;
+double  r = 2;
+
+  double Areaofcirlce = pai*(r*r);
+
+System.out.println("Areaofcirlce :"+Areaofcirlce);
+}
+}
